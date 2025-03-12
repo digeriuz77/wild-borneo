0d6d8a330605bc11a8b19f49d77283a10e480138
digeriuz77 <109959055+digeriuz77@users.noreply.github.com>
2025-03-12 11:48:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -797,9 +797,10 @@ def show_identification():
             </div>
             """, unsafe_allow_html=True)
     else:
-    # Next button
-    if st.button("Next Species", key="next_species_button"):
-        st.session_state.answer_checked = False
+
+# Next button
+if st.button("Next Species", key="next_species_button"):
+    st.session_state.answer_checked = False
     
     # Check if all species have been shown
     if len(st.session_state.species_done) >= len(species_list):
